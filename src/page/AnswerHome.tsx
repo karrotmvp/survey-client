c3d660c2d33b45343cef06856622fb3983713ff9
Dongmin Ahn <eamon3481@naver.com>
2021-11-01 16:54:18 +0900

    Chore : console 제거

--- src/page/AnswerHome.tsx
+++ src/page/AnswerHome.tsx
@@ -53,14 +53,12 @@ export default function AnswerHome(): JSX.Element {
 
   const click = async () => {
     const resCode = await auth();
-    console.log(resCode);
     if (resCode) {
       setCode(resCode);
     }
   };
 
   useEffect(() => {
-    console.log(isSuccess);
     if (isSuccess) {
       getSurveyData().then(data => {
         if (!data) return;
@@ -70,6 +68,7 @@ export default function AnswerHome(): JSX.Element {
         push(`/responses/${responseId}/1`);
       });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isSuccess, code]);
 
   // const responsId = 162;
