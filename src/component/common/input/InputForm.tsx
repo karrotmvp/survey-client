e7726e65d25e885bd237d911ab2444651c771917
Dongmin Ahn <eamon3481@naver.com>
2021-12-06 17:37:00 +0900

    fix : 빌드에러

--- src/component/common/input/InputForm.tsx
+++ src/component/common/input/InputForm.tsx
@@ -51,8 +51,8 @@ export type RegisterOptions = Partial<{
   required: Message | ValidationRule<boolean>;
   min: ValidationRule<number | string>;
   max: ValidationRule<number | string>;
-  maxLength: ValidationRule<number | string>;
-  minLength: ValidationRule<number | string>;
+  maxLength: ValidationRule<number>;
+  minLength: ValidationRule<number>;
   pattern: ValidationRule<RegExp>;
 }>;
 
